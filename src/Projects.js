f0ba1e8f9c14d9f37d249947a905dbf689237327
Tommy Le <tcdle137@gmail.com>
2025-10-06 00:10:23 -0400

    language fixes and hyperlinked projects

--- src/Projects.js
+++ src/Projects.js
@@ -9,14 +9,14 @@ import p3_2 from './p3_2.png';
 function Projects() {
   const projectData = [
     {
-      title: 'Wordle Bot (found on my GitHub)',
-      description: 'This personal project focused on creating a bot that would play the New York Times daily game, Wordle. Through the use of writing algorithms to strategically reduce the number of possible words, I tried to minimize the number of guesses that the bot would make. During the development process, I practiced debugging techniques and considered time complexity when writing the algorithms. I ran the bot for over thousands of games and took the average, number of guesses, maximum/minimum number of guesses, win percentage, and runtime. The Wordle bot was coded in C++ and can be found on my GitHub.',
+      title: <a href="https://github.com/tcdle137/WordleBot" target="_blank" rel="noopener noreferrer">Wordle Bot</a>,
+      description: 'This personal project focused on creating a bot that would play the New York Times daily game, Wordle. Through the use of writing algorithms to strategically reduce the number of possible words, I tried to minimize the number of guesses that the bot would make. During the development process, I practiced debugging techniques and considered time complexity when writing the algorithms. I ran the bot for over thousands of games and took the average, number of guesses, maximum/minimum number of guesses, win percentage, and runtime. The Wordle bot was coded in C++ and can be found on my GitHub (click on the section title to be sent to the project repository).',
       image: p1_1,
       image2: p1_2
     },
     {
-      title: 'Interactive Store (found on my GitHub)',
-      description: 'This project was the final project of my Java course at LTU. It aimed to create an interactive and functional UI for a theoretical store. The UI would calculate the total price of the items, apply discounts and taxes when applicable, then either display the reciebt or save it to a file. The project focused on input validation, Object-Oriented Design, and front/back-end development. The Interactive Store was coded in Java and can be found on my GitHub.',
+      title: <a href="https://github.com/tcdle137/Java_Store" target="_blank" rel="noopener noreferrer">Interactive Store</a>,
+      description: 'This project was the final project of my Java course at LTU. It aimed to create an interactive and functional UI for a theoretical store. The UI would calculate the total price of the items, apply discounts and taxes when applicable, then either display the reciebt or save it to a file. The project focused on input validation, Object-Oriented Design, and front/back-end development. The Interactive Store was coded in Java and can be found on my GitHub (click on the section title to be sent to the project repository).',
       image: p2_1,
       image2: p2_2,
     },
