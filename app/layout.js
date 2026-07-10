2f2a746aa90f3b6dff45d0f2e284af35eeb9eabd
Tommy Le <tcdle137@gmail.com>
2026-07-10 15:54:47 -0500

    im stupid

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,17 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="body">
+      <body style={{ 
+        backgroundImage: `url(${background.src})`, 
+        backgroundColor: '#1c1c1c',
+        backgroundSize: '100% 100%',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center',
+        minHeight: '100dvh',
+        overflow: 'auto',
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
         {children}
         <Footer />
       </body>
